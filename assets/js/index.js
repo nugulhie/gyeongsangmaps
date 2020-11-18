5321593765b134cc7ad1ab36e840258ac45d3d55
DoubleY Hwnag <thwang@gmail.com>
2020-11-18 15:52:59 +0900

    1. 파이어스토어에서 마커에 데이터 및 댓글 불러오기 2. 작성하기 구현중

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+
 // Initialize Firebase
 var firebaseConfig = {
   apiKey: "AIzaSyDahWd_FZdoPEazahqR8aFgZvgWgWtZ_3U",
@@ -21,10 +22,11 @@ db.collection('cafes').get().then(snapshot => {
   snapshot.docs.forEach(doc => {
       console.log(doc.data());
       username = doc.get("name");
-      
   });
 });
 
+
+//
 
 
 //test
@@ -48,28 +50,53 @@ function initMap() {
     center: university,
   });
 
-
-  
-  
 //DB에서 값 받아오고 마커 넣어줌.
   db.collection("cafes").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
+        
+        var name = doc.data().name;
+        var title= doc.data().title;
+        var arr = doc.data().comments;
+        var latitude = doc.data().geopoint.latitude;
+        var longitude = doc.data().geopoint.longitude;
         console.log(`${doc.id} => ${doc.data()}`);
-        var name = doc.data().name
-        var latitude = doc.data().geopoint.latitude
-        var longitude = doc.data().geopoint.longitude
-        console.log(latitude)
-        console.log(longitude)
+        console.log(latitude);
+        console.log(longitude);
+        console.log(title);
+        console.log(name);
+        
         const mark1 = { lat: latitude, lng: longitude };
+       
+        var contentString = 
 
-        const contentString = name 
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        "</div>" +
+        '<h4 id="firstHeading" class="firstHeading">'+ title + '</h4>' +
+        '<div id="bodyContent">' +
+        "<p>" + name + "</p>" +
+        '<a href="#">자세히 보기 </a></br>'+
+        "<button>수정 하기</button>" + 
+        "</div>" + 
+        "</div><br>" ;
+
+        for(var i = 0 ; i < arr.length ; i++){
+          contentString = contentString + "<p>" + "<b>" + arr[i].id + "</b>" +": " + arr[i].comment + "</p>";
+        }
+
+        contentString += 
+        '<textarea id="w3review" name="w3review" rows="1" cols="30"></textarea><br>' + 
+        "<button>댓글 달기</button>";
+
+        
         const infowindow = new google.maps.InfoWindow({
           content: contentString,
         });
+        
         const marker = new google.maps.Marker({
           position: mark1,
           map,
-          title: "경상대학교 테스트 제목",
+          title: title,
         });
         //addMarker(latLng)
         marker.addListener("click", () => {
@@ -79,19 +106,7 @@ function initMap() {
 });
 //DB 끝
 
-  const contentString = username
-      
-    const infowindow = new google.maps.InfoWindow({
-      content: contentString,
-    });
-    const marker = new google.maps.Marker({
-      position: university,
-      map,
-      title: "경상대학교 테스트 제목",
-    });
-    marker.addListener("click", () => {
-      infowindow.open(map, marker);
-    });
+
   // This event listener will call addMarker() when the map is clicked.
   map.addListener("click", (event) => {
     addMarker(event.latLng);
@@ -104,21 +119,44 @@ function addMarker(location) {
     position: location,
     map: map,
   });
+  console.log(typeof(location));
   markers.push(marker);
   marker.addListener("click", () => {
     infowindow.open(map, marker);
   });
-  const contentString = username
-      
-
-
+  console.log(location.lng());
     const infowindow = new google.maps.InfoWindow({
-      content: contentString,
-      
-    });
-    
-}
+      content: 
+      '<form action="" id="newActivity">' + 
+        '<header>' +
+          '<h4> 장소 공유</h4>' +
+          '<div> 당신만에 장소를 공유하세요 </div>'+
+        '</header>' +
+      '  <div>'+
+      '    <label class="desc" id="title1" for="Field1">장소 이름:' +
+      '    </label>'+
+      '    <div>'+
+      '      <input id="activityTitle" name="Field1" type="text" value="" tabindex="1">'+
+      '    </div>'+
+      '  </div>'+
+      '  <div>'+
+      '    <label class="desc" id="description1" for="Field2">장소를 설명해주세요!: </label>'+
+      '    <div>'+
+      '      <textarea id="activityDescription" name="Field2" spellcheck="true" rows="10" col="50" tabindex="2" value="default">'+
+      '      </textarea>'+
+      '    </div>'+
+      '   </div>'+
+      '  <br />'+
+      '</div> '+
+      '</form>'+
+      '  <div>'+
+      '    <button onclick="myFunction(location)">공유 하기</input>'+
+      '  </div>'
 
+    });
+
+  }
+  
 // Sets the map on all markers in the array.
 function setMapOnAll(map) {
   for (let i = 0; i < markers.length; i++) {
@@ -142,3 +180,18 @@ function deleteMarkers() {
   markers = [];
 }
 
+
+function myFunction(location) { 
+      db.collection("cafes").add({ 
+           title: document.getElementById('activityTitle').value, 
+          name: document.getElementById('activityDescription').value, 
+          geopoint : new firebase.firestor.GeoPoint(location.lat(), location.lng())
+        }).then(function() { 
+        console.log("Document successfully wriiten!"); 
+  }) 
+  .catch(function(error) {
+      console.error("Error writing document: ", error);
+      let coords = []
+coords.push(new firebase.firestore.GeoPoint(form.lat.value, form.long.value))
+  });
+    }
