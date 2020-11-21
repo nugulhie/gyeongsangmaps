ae323db6580d7bd24bd801992c405de351bdbb49
GongWook <rhdwhddnr@gmail.com>
2020-11-22 02:54:37 +0900

    newplace 지정시 팝업 css 적용 완료

---
+++ assets/js/newplacepopup.js
@@ -0,0 +1,49 @@
+function addMarker(location) {
+    const iconBase ="/img/booot.png";
+    const marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      icon: iconBase
+    });
+    const infowindow = new google.maps.InfoWindow({
+      content: 
+      '<div class="popup" id="newActivity">'+
+        '<form action="">'+
+          '<h1 class="h1">나만의 장소</h1>'+
+          '<h4 class="h4">당신의 추억을 공유해 주세요.</h4>'+
+          '<div class="cont">'+
+            '<table class="type1>'+
+              '<colgroup>'+
+                '<col style="width: 111px">'+
+                '<col>'+
+              '</colgroup>'+
+              '<tbody>'+
+              '<tr>'+
+                '<th>장소 이름<span>*</span></th>'+
+                '<td>'+'<input type="text" id="activityTitle" placeholder="장소를 입력해 주세요.">'+'</td>'+
+              '</tr>'+
+              '<tr>'+
+                '<th><label for=""></label></th>'+
+                '<td><textarea id="activityDescription" placeholder="장소 설명을 부탁드릴게요."></textarea></td>'+
+              '</tr>'+
+              '</tbody>'+
+            '</table>'+
+          '</div>'+
+
+            '<div class="txt_center">'+
+                '<button onclick="onClickSubmit()" class="btn_type1">공유 하기</input>'+
+            '</div>'+
+        '</form>'+
+      '</div>'
+    });
+  
+    markers.push(marker);
+    marker.addListener("click", () => {
+  
+      infowindow.open(map, marker);
+      temp_infowindow = infowindow;
+    });
+    //console.log(location.lng());
+      
+      
+    }
