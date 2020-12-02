2e4b21fce819104b507f03d5ec417e352bb2b59b
SH22Hwang <ghkd3531@naver.com>
2020-12-02 21:40:43 +0900

    edit 완성 90%

--- assets/js/edit.js
+++ assets/js/edit.js
@@ -1,21 +1,15 @@
 //프로필 수정 페이지
-var my_name = document.getElementById('my_name');
-var my_college= document.getElementById('my_college');
-var my_context= document.getElementById('my_context');
+function update(){
+  var my_name = document.getElementById('my_name').value;
+  var my_college = document.getElementById('my_college').value;
+  var my_context = document.getElementById('my_context').value;
 
-db.collection('users').doc(user.uid).get().then(function(doc) {
-  if (doc.exists) {
-      console.log("Document data:", doc.data());
-      console.log(doc.data());
-      my_name.innerHTML = doc.get("name");
-      my_college.innerHTML = doc.get("college");
-      my_context.innerHTML = doc.get("context");
-      
-  } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-  }
-}).catch(function(error) {
-  console.log("Error getting document:", error);
-      console.log(my_name, my_college, my_context);
+  db.collection('users').doc(user.uid).set().then(function(doc) {
+    name: my_name;
+    college: my_college;
+    context: my_context;
+  }).catch(function(error) {
+    console.log("Error Writing document:", error);
+    console.log(my_name, my_college, my_context);
   });
+}
--- assets/js/profile.js
+++ assets/js/profile.js
@@ -16,9 +16,9 @@ var userRef = db.collection('users').doc('user');
 // doc("value 값에 userToken을 넣어야한다.")
 // uid를 넣었을때 작동 안됨
 // 프로필 수정에서 값 넘겨주는 method
-
-
 const auth = firebase.auth();
+var user = auth.currentUser;
+var userRef = db.collection('users').doc(user.uid);
 
 //현재 로그인 된 아이디의 상태 값을 가져온다. 로그인 상태면 True를 리턴. 아니면 False를 리턴한다.
 auth.onAuthStateChanged(function(user){
@@ -30,7 +30,8 @@ auth.onAuthStateChanged(function(user){
     var user_college= document.getElementById('user_college');
     var user_age= document.getElementById('user_age');
     var user_context= document.getElementById('user_context');
-
+      
+      // auth에 저장된 정보 (회원가입시 입력받은 정보)
       //user_name.innerHTML = user.name
       //user_age.innerHTML = user_age
       //user_college.innerHTML = user_college
@@ -41,14 +42,13 @@ auth.onAuthStateChanged(function(user){
       // userData에서 users로 변경
       db.collection('users').doc(user.uid).get().then(function(doc) {
         if (doc.exists) {
-            console.log("Document data:", doc.data());
-            console.log(doc.data());
-            user_name.innerHTML = doc.get("name");
-            user_age.innerHTML = doc.get("age");
-            user_college.innerHTML = doc.get("college");
-            //user_email.innerHTML = doc.get("email");
-            user_context.innerHTML = doc.get("context");
-            
+          console.log("Document data:", doc.data());
+          console.log(doc.data());
+          user_name.innerHTML = doc.get("name");
+          user_age.innerHTML = doc.get("age");
+          user_college.innerHTML = doc.get("college");
+          //user_email.innerHTML = doc.get("email");
+          user_context.innerHTML = doc.get("context");
         } else {
             // doc.data() will be undefined in this case
             console.log("No such document!");
@@ -63,84 +63,31 @@ auth.onAuthStateChanged(function(user){
   }
 });
 
-function update(){
-  console.log("버튼을 누름");
-  var name = document.getElementById('user_name').value;
-  var id = document.getElementById('user_email').value;
-  var context = document.getElementById('user_context').value;
-  console.log(name, id, context);
-  return userRef.update({
-    name: name,
-    email: id,
-    context: context
-  })
-  .then(function() {
-    
-      console.log("Document successfully updated!");
-  })
-  .catch(function(error) {
-      // The document probably doesn't exist.
-      console.error("Error updating document: ", error);
-  });
-}
-
-//docRef.id 값을 회원가입할때 부터 받아서 파이어스토어 연동시 입력해주어야 함.
-// if (window.performance) {
-//   console.info("window.performance work's fine on this browser");
-// }
-//   if (performance.navigation.type == 1) {
-//     console.info( "This page is reloaded" );
-//   } else {
-//     console.info( "This page is not reloaded");
-//   }
-
-
-function readData(){
-var user_name = document.getElementById('user_name');
-var user_email= document.getElementById('user_email');
-var user_college= document.getElementById('user_college');
-var user_age= document.getElementById('user_age');
-var user_context= document.getElementById('user_context');
-// doc('user')값에서 user 대신 고유 id값으로 넣어줄 예정.
-// db.collection('users').doc('user').get().then(function(doc) {
-//   if (doc.exists) {
-//       console.log("Document data:", doc.data());
-//       console.log(doc.data());
-//       user_name.innerHTML = doc.get("name");
-//       user_age.innerHTML = doc.get("age");
-//       user_college.innerHTML = doc.get("college");
-//       user_email.innerHTML = doc.get("email");
-//       user_context.innerHTML = doc.get("context");
-      
-//   } else {
-//       // doc.data() will be undefined in this case
-//       console.log("No such document!");
-//   }
-// }).catch(function(error) {
-//   console.log("Error getting document:", error);
-//       console.log(user_name, user_age, user_college, user_email);
-//   });
+function current(){
+  document.getElementById("current_name").innerHTML = userRef;
   
 }
 
-// //프로필 수정 페이지 edit.js로 이동
-// var my_name = document.getElementById('my_name');
-// var my_college= document.getElementById('my_college');
-// var my_context= document.getElementById('my_context');
 
-// db.collection('users').doc(user.uid).get().then(function(doc) {
-//   if (doc.exists) {
-//       console.log("Document data:", doc.data());
-//       console.log(doc.data());
-//       my_name.innerHTML = doc.get("name");
-//       my_college.innerHTML = doc.get("college");
-//       my_context.innerHTML = doc.get("context");
-      
-//   } else {
-//       // doc.data() will be undefined in this case
-//       console.log("No such document!");
-//   }
-// }).catch(function(error) {
-//   console.log("Error getting document:", error);
-//       console.log(my_name, my_college, my_context);
-//   });
+function update(){
+  var user = auth.currentUser;
+
+  var my_name = document.getElementById('my_name').value;
+  var my_college = document.getElementById('my_college').value;
+  var my_context = document.getElementById('my_context').value;
+  console.log(my_name, my_college, my_context);
+
+  var userRef = db.collection('users').doc(user.uid);
+  return userRef.update({
+    name: document.getElementById('my_name').value,
+    college: document.getElementById('my_college').value,
+    context: document.getElementById('my_context').value
+  })
+  .then(function(){
+    console.log("Document successfully written!");
+  })
+  .catch(function(error) {
+    console.log("Error Writing document:", error);
+  });
+}
+
