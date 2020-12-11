aaf99c7977c1b5451d3b9b0c7b62cffca91fafc3
SH22Hwang <ghkd3531@naver.com>
2020-12-11 12:52:08 +0900

    ui 바꿈. 카드 프로필 보이게함

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -26,63 +26,45 @@ if(user){ //로그인 된 상태
   var user_age= document.getElementById('user_age');
   var user_context= document.getElementById('user_context');
 
-    //user_name.innerHTML = user.name
-    //user_age.innerHTML = user_age
-    //user_college.innerHTML = user_college
-    user_email.innerHTML = user.email
-    //user_context.innerHTML = user_context
-
-    // 파이어베이스에서 userData collection의 user.uid가 일치하는 문서를 찾아 그 정보를 반환한다.
-    // Use User.getToken() instead. 권장사항
-    db.collection('users').doc(user.uid).get().then(function(doc) {
-      if (doc.exists) {
-          console.log("Document data:", doc.data());
-          console.log(doc.data());
-          user_name.innerHTML = doc.get("name");
-          user_age.innerHTML = doc.get("age");
-          user_college.innerHTML = doc.get("college");
-          //user_email.innerHTML = doc.get("email");
-          user_context.innerHTML = doc.get("context");
-          
-      } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-      }
-    }).catch(function(error) {
-      console.log("Error getting document:", error);
-          console.log(user_name, user_age, user_college, user_email);
-      });
-
-}else{//로그아웃 된 상태.
-  console.log(user);
-}
-});
-
-function init_edit(){
-  var current = auth.currentUser;
-  if (current != null) {
-    name = current.displayName;
-  }
   var current_name = document.getElementById('my_name');
   var current_college = document.getElementById('my_college');
   var current_context = document.getElementById('my_context');
 
+  user_email.innerHTML = user.email
+
+  // 파이어베이스에서 userData collection의 user.uid가 일치하는 문서를 찾아 그 정보를 반환한다.
+  // Use User.getToken() instead. 권장사항
   db.collection('users').doc(user.uid).get().then(function(doc) {
     if (doc.exists) {
-      getElementById('current_name').innerHTML = 
-        '<h4 class="pt-sm-2 pb-1 mb-0 text-nowrap">' + name + '</h4>';
-      current_name.setAttribute('value', doc.get("name"));
-      current_college.setAttribute('value', doc.get("college"));
-      current_context.innerHTML = 
-      '<label>프로필 설명</label>' + '<textarea class="form-control" rows="5" placeholder="프로필 설명" id="current_context">'+ doc.get("context") +'</textarea>';
-    } else {
-        console.log("No such document!");
+      console.log("Document data:", doc.data());
+      console.log(doc.data());
+      user_name.innerHTML = doc.get("name");
+      user_age.innerHTML = doc.get("age");
+      user_college.innerHTML = doc.get("college");
+      //user_email.innerHTML = doc.get("email"); 위에서 이미 정의
+      user_context.innerHTML = doc.get("context");
+          
+      document.getElementById('card_profile').innerHTML = doc.get("context");
+      // 카드 텍스트에 프로필 메시지 띄움
+      // profile.ejs에서는 적용되지만, edit.ejs로 넘어가면 적용안됨
+
+      current_name.value = doc.get("name");
+      current_college.value = doc.get("college");
+      current_context.value = doc.get("context"); // 안됨
+      
+    }else{
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
     }
   }).catch(function(error) {
     console.log("Error getting document:", error);
-        console.log(user_name, user_age, user_college, user_email);
-  });  
-}
+    console.log(user_name, user_age, user_college, user_email);
+  });
+
+  }else{//로그아웃 된 상태.
+    console.log(user);
+  }
+});
 
 // edit.ejs에서 저장 버튼 누르면 실행
 // 기존 문서 없으면 실행안됨?
@@ -96,7 +78,21 @@ function update(){
   console.log(my_name, my_college, my_context);
   
   userRef.get().then(function() {
-  //   if (doc.exists){  //문서 있음
+    return userRef.update({
+      name: document.getElementById('my_name').value,
+      college: document.getElementById('my_college').value,
+      context: document.getElementById('my_context').value
+    })
+    .then(function(){
+      console.log("Document successfully written!");
+      alert("저장되었습니다.");
+      window.location.replace("myprofile");
+    })
+    .catch(function(error) {
+      console.log("Error Writing document:", error);
+    });
+  })
+    //   if (doc.exists){  //문서 있음
   //     return userRef.update({
   //       name: document.getElementById('my_name').value,
   //       college: document.getElementById('my_college').value,
@@ -117,16 +113,5 @@ function update(){
   //   console.log("Error setting document:", error);
   //   console.log(my_name, my_college, my_context);
   // });
-  
-  return userRef.update({
-    name: document.getElementById('my_name').value,
-    college: document.getElementById('my_college').value,
-    context: document.getElementById('my_context').value
-  })
-  .then(function(){
-    console.log("Document successfully written!");
-  })
-  .catch(function(error) {
-    console.log("Error Writing document:", error);
-  });
-})}
+  // 회원가입할때 프로필을 미리 작성하는 페이지가 있으면 좋겠음.
+}
